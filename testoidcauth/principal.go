8b2190e4d1bd6cf283b4a2afcd605d84cbb185fd
hbc <bcxxxxxx@gmail.com>
2022-06-28 14:09:21 +0800

    test: add testsoidcauth package

---
+++ testoidcauth/principal.go
@@ -0,0 +1,54 @@
+package testoidcauth
+
+import "github.com/b4fun/oidcauth"
+
+// ClaimsPrincipal provides on-demand mock for oidcauth.ClaimsPrincipal type.
+type ClaimsPrincipal struct {
+	NameFunc            func() string
+	HasRoleFunc         func(role string) bool
+	ClaimsFunc          func() oidcauth.MapClaims
+	BindClaimsFunc      func(v interface{}) error
+	AuthenticateErrFunc func() error
+}
+
+var _ oidcauth.ClaimsPrincipal = (*ClaimsPrincipal)(nil)
+
+func (cp *ClaimsPrincipal) Name() string {
+	if cp.NameFunc != nil {
+		return cp.NameFunc()
+	}
+
+	panic("not implemented")
+}
+
+func (cp *ClaimsPrincipal) HasRole(role string) bool {
+	if cp.HasRoleFunc != nil {
+		return cp.HasRoleFunc(role)
+	}
+
+	panic("not implemented")
+}
+
+func (cp *ClaimsPrincipal) Claims() oidcauth.MapClaims {
+	if cp.ClaimsFunc != nil {
+		return cp.ClaimsFunc()
+	}
+
+	panic("not implemented")
+}
+
+func (cp *ClaimsPrincipal) BindClaims(v interface{}) error {
+	if cp.BindClaimsFunc != nil {
+		return cp.BindClaimsFunc(v)
+	}
+
+	panic("not implemented")
+}
+
+func (cp *ClaimsPrincipal) AuthenticateErr() error {
+	if cp.AuthenticateErrFunc != nil {
+		return cp.AuthenticateErrFunc()
+	}
+
+	panic("not implemented")
+}
